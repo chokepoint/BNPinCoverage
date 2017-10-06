b850695145cbb7dfa7fb0d413de62e836714e1d5
stderr <stderr@chokepoint.net>
2017-10-06 11:53:49 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# BN Pin Coverage (v1.0 alpha)
+Author: **Chokepoint**
+
+_Import Pin Tool basic block trace data and highlight executed basic blocks._
+
+## Description:
+
+Use this tool to import Pin trace data using a custom plugin that prints the addresses of executed basic blocks. This can help isolate dead blocks of code or give a good idea of code coverage for test cases.
+
+## Minimum Version
+
+This plugin requires the following minimum version of Binary Ninja:
+
+ * release - 9999
+ * dev - 1.0.dev-576
+
+
+## Required Dependencies
+
+The following dependencies are required for this plugin:
+
+ * pip - binaryninja
+ * installers - 
+ * other - The plugin requires [Pin tool](https://software.intel.com/en-us/articles/pin-a-binary-instrumentation-tool-downloads) be installed.
+ * apt - 
+
+
+## License
+
+This plugin is released under a [MIT](LICENSE) license.
+
+
---
+++ __init__.py
@@ -0,0 +1,49 @@
+from binaryninja import *
+
+def reset_highlight(view):
+    blockcount = 0.0
+    for func in view.functions:
+        for bb in func.basic_blocks:
+            bb.highlight = HighlightStandardColor.NoHighlightColor
+            blockcount += 1
+    view.session_data['blockcount'] = blockcount
+
+def import_pin_trace(view):
+    reset_highlight(view)
+
+    e_types = ['ET_NONE', 'ET_REL', 'ET_EXEC', 'ET_DYN', 'ET_CORE']
+    addrs = []
+    offset = 0
+            
+    filename = get_open_filename_input("Import Pin Trace", "*.ptrace")
+
+    # If this is a PIE executable, grab the ELF entry point for calculating the slide
+    if view.view_type == 'ELF':
+        elf_type = e_types[ord(view.read(0x10, 1))]
+        if elf_type == 'ET_DYN':
+            start_loc = view.entry_point
+            
+    with open(filename) as fd:
+        first_line = True
+        for line in fd:
+            if line.startswith("BB:"):
+                addr = int(line.split(' ', 1)[1], 16)
+                if first_line and elf_type == 'ET_DYN':
+                    offset = addr - start_loc
+                    first_line = False
+                addrs.append(addr - offset)
+
+    addrs.sort()
+    uaddrs = set(addrs)
+
+    for addr in uaddrs:
+        try:
+            bb = view.get_basic_blocks_at(addr)[0]
+            bb.highlight = highlight.HighlightStandardColor.CyanHighlightColor
+        except Exception:
+            pass
+    blockcount = view.session_data['blockcount']
+    show_message_box("Coverage", "Basic blocks hit (%d / %d) = %.2f" % (len(uaddrs), blockcount, (len(uaddrs) / blockcount * 100)))
+
+PluginCommand.register("Clear Pin Highlighting", "Remove highlighting from every basic block", reset_highlight)
+PluginCommand.register("Import Pin Trace", "Import a Pin Trace", import_pin_trace)
